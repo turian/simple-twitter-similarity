460f03a226b0cebbcf1d4c0729a2647ff526b6d6
Joseph Turian <turian@gmail.com>
2010-04-28 17:30:04 -0400

    Compute similarity, wrote some documentation

--- simple-twitter-similarity.py
+++ simple-twitter-similarity.py
@@ -2,25 +2,65 @@
 
 import twitter
 
+# Some extra stopwords to remove
+STOPWORDS = ["http", "rt", "//bit.ly/", "RT"]
+
 def preprocess(text, preprocessing):
     """
     Preprocess the text.
         preprocessing = None or 'string.split' means simply use string.split.
         preprocessing = pytextpreprocess means use the pytextpreprocess library.
     """
+    import string
     if preprocessing is None or preprocessing == "string.split":
-        import string
         return string.split(text)
     elif preprocessing == "pytextpreprocess":
         import textpreprocess
-        return textpreprocess.textpreprocess(text)
+        return string.split(textpreprocess.textpreprocess(text))
 
 def representation(statuses, preprocessing):
     """
     Get the representation for a particular user, as represented by a list of statuses.
+    This representation is given as a dict: term=>value
     """
+    from collections import defaultdict
+    repr = defaultdict(int)
     for s in statuses:
-        print preprocess(s.text, preprocessing)
+        for t in preprocess(s.text, preprocessing):
+            if t in STOPWORDS: continue
+            repr[t] += 1
+    return repr
+
+def cosine(repr1, repr2):
+    """
+    Compute the Cosine similarity between repr1 and repr2 (http://en.wikipedia.org/wiki/Cosine_distance).
+    Each repr is given as a dict: term=>value.
+
+    Note: We could also use scipy.sparse operations, which are more
+    concise, but this would require constructing a term=>id map. See
+    common.idmap in my Python common package.
+    """
+    import math
+    norm1 = 0.
+    for t in repr1: norm1 += repr1[t] * repr1[t]
+    norm1 = math.sqrt(norm1)
+    norm2 = 0.
+    for t in repr2: norm2 += repr2[t] * repr2[t]
+    norm2 = math.sqrt(norm2)
+
+#    from common.mydict import sort as dictsort
+#    print dictsort(repr1)[:10]
+#    print dictsort(repr2)[:10]
+
+    import sets
+    allterms = sets.ImmutableSet(repr1.keys() + repr2.keys())
+
+    sim = 0.
+    for t in allterms:
+#        print "%.3f = %.3f * %.3f, %s" % (repr1[t] * repr2[t],repr1[t], repr2[t], t.encode("utf-8"))
+        sim += (1. * repr1[t] * repr2[t] / (norm1 * norm2))
+
+    return sim
 
 _api = None
 def similarity(user1, user2, preprocessing=None):
@@ -38,8 +78,10 @@ def similarity(user1, user2, preprocessing=None):
     print >> sys.stderr, "Loaded %d statuses for %s" % (len(statuses1), user1)
     print >> sys.stderr, "Loaded %d statuses for %s" % (len(statuses2), user2)
 
-    print representation(statuses1, preprocessing)
-    print representation(statuses2, preprocessing)
+    repr1 = representation(statuses1, preprocessing)
+    repr2 = representation(statuses2, preprocessing)
+
+    return cosine(repr1, repr2)
 
 if __name__ == "__main__":
     import sys
